69066515f6931a47e81484a3d4c28c642b62dad7
Prince Krishna Adithya <78862316+PrinceKrishnaAdithya@users.noreply.github.com>
2026-02-04 01:54:34 +0530

    Create BD_Q9.py

---
+++ BD_Q9.py
@@ -0,0 +1,54 @@
+"""
+🌍 Question 9: Smart City Telecom Readiness Index (SCTRI)
+📌 Problem Statement
+Identify regions ready for smart-city telecom upgrades.
+
+🧠 Concept
+SCTRI = Residential + (2 × Towers)
+"""
+
+from pymongo import MongoClient
+from urllib.parse import quote_plus
+
+
+uri = "mongodb+srv://princekrishnaadi_db_user:lr41c9iGRoOX8vnk@telecomcluster.rzvshu0.mongodb.net/"
+client = MongoClient(uri)
+
+db = client["BigData_Spatial"]
+
+residential = db.residential_clean
+towers = db.towers_clean
+
+states = db.states_clean          # District polygons
+towers = db.towers_clean          # Telecom tower points
+res    = db.residential_clean     # Residential points
+
+
+grid_points = [
+    [76.94, 11.00], [76.96, 11.00], [76.98, 11.00], [77.00, 11.00],
+    [76.94, 11.02], [76.96, 11.02], [76.98, 11.02], [77.00, 11.02],
+    [76.94, 11.04], [76.96, 11.04], [76.98, 11.04], [77.00, 11.04],
+]
+
+EARTH_RADIUS_KM = 6378.1
+RADIUS_KM = 5
+RADIUS_RAD = RADIUS_KM / EARTH_RADIUS_KM
+
+
+from shapely.geometry import shape, Point
+
+results = []
+
+for p in grid_points:
+    r = residential.count_documents({
+        "geometry": {"$geoWithin": {"$centerSphere": [p, RADIUS_RAD]}}
+    })
+    t = towers.count_documents({
+        "geometry": {"$geoWithin": {"$centerSphere": [p, RADIUS_RAD]}}
+    })
+
+    sctri = r + (2 * t)
+    results.append({"center": p, "SCTRI": sctri})
+
+results.sort(key=lambda x: x["SCTRI"], reverse=True)
+print(results[0])
